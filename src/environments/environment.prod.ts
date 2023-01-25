e1e992dd3ec7bc682cbd69ee64369002d661db89
mikita <popok11014@tut.by>
2023-01-25 13:52:26 +0400

    add config for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyCOZ-8vBtjxMgh3zLHV1O3xpauzRRDQ-FE',
+    authDomain: 'cosinuts-e9d48.firebaseapp.com',
+    databaseURL: 'https://cosinuts-e9d48-default-rtdb.firebaseio.com',
+    projectId: 'cosinuts-e9d48',
+    storageBucket: 'cosinuts-e9d48.appspot.com',
+    messagingSenderId: '208308027771',
+    appId: '1:208308027771:web:68e7e790245fd6e3f57dde',
+    measurementId: 'G-XTVZN1FLJ1',
+  },
 };
