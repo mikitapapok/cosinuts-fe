0e8d9d408036fb308457a43dca5f634bec4f7416
mikita <popok11014@tut.by>
2023-01-25 12:04:58 +0400

    delete productType

--- src/app/shared/categories/categories.component.ts
+++ src/app/shared/categories/categories.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {
   categoriesList,
   PageTitles,
   ProductTypesType,
 } from '../constants/contstans';
-import { AppStateInterface, ICategories } from '../interfaces/interfaces';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { changeTypeAction } from '../../store/product-type-store/product-type.actions';
-import { productTypeSelector } from '../../store/product-type-store/product-type.selectors';
+import {AppStateInterface, ICategories} from '../interfaces/interfaces';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {changeTypeAction} from '../../store/product-type-store/product-type.actions';
+import {productTypeSelector} from '../../store/product-type-store/product-type.selectors';
 import {
   addProductsAction,
   runLoadingAction,
@@ -22,23 +22,22 @@ export class CategoriesComponent implements OnInit {
   title?: string;
   categoriesList?: ICategories[];
   productTypeSubscription$?: Observable<string>;
-  productType?: ProductTypesType;
 
   constructor(private store: Store<AppStateInterface>) {
     this.title = PageTitles.Categories;
     this.categoriesList = categoriesList;
   }
+
   ngOnInit() {
     this.productTypeSubscription$ = this.store.pipe(
       select(productTypeSelector)
     );
-    this.store.dispatch(addProductsAction({ offset: 0 }));
+    this.store.dispatch(addProductsAction({offset: 0}));
   }
 
   changeProductType(typeOfProduct: ProductTypesType): void {
-    this.productType = typeOfProduct;
     this.store.dispatch(runLoadingAction());
-    this.store.dispatch(changeTypeAction({ productType: typeOfProduct }));
-    this.store.dispatch(addProductsAction({ offset: 0 }));
+    this.store.dispatch(changeTypeAction({productType: typeOfProduct}));
+    this.store.dispatch(addProductsAction({offset: 0}));
   }
 }
